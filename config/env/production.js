0621f42f20fbf6689ed12efba24e0669a5715f64
kothavaleganesh <ganesh.wohlig@gmail.com>
2017-08-25 15:35:01 +0530

    changed IP

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 80,
-  realHost: "http://35.189.189.254",
+  realHost: "http://35.194.160.101",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com", "nargis.shaikh@wohlig.com"]
 
   /***************************************************************************
