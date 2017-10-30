c69f184d630a74d01c1ea965673368a3ee94578c
Aditya <aditya.ghag@wohlig.com>
2017-10-30 16:44:15 +0530

    mailer integration

--- api/services/Enquiry.js
+++ api/services/Enquiry.js
@@ -20,52 +20,5 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('Enquiry', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {
-
-    sendEnquiry: function (data, callback) {
-        console.log("data", data);
-        // var contact = this(data);
-        var match = {};
-        if (data.email) {
-            match = {
-                email: data.email
-            };
-        } else {
-            match = {
-                _id: data._id
-            };
-        }
-        Enquiry.findOne(match).exec(function (err, data1) {
-            console.log("data1", data1, err);
-            if (err) {
-                callback(err, null);
-            } else if (data1) {
-                console.log("data", data1);
-                var emailData = {}
-                emailData.email = data1.email;
-                // emailData.mobile = data1.mobile;
-                emailData.filename = "Inquiry";
-                emailData.name = data1.name;
-                emailData.subject = "Inquiry Details";
-                console.log("email data : ", emailData);
-
-                Config.email(emailData, function (err, emailRespo) {
-                    console.log("emailRespo", emailRespo);
-                    if (err) {
-                        console.log(err);
-                        callback(err, null);
-                    } else if (emailRespo) {
-                        callback(null, "Contact us form saved successfully!!!");
-                    } else {
-                        callback("Invalid data", null);
-                    }
-                });
-
-            } else {
-                callback("Invalid data", null);
-            }
-        });
-    },
-
-};
+var model = {};
 module.exports = _.assign(module.exports, exports, model);
