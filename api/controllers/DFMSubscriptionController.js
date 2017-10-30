feb3bf753f8b28b5567f691c58cd2e4992f69646
kothavaleganesh <ganesh.wohlig@gmail.com>
2017-10-30 18:49:44 +0530

    stored zoom level to restrict view

--- api/controllers/DFMSubscriptionController.js
+++ api/controllers/DFMSubscriptionController.js
@@ -1,7 +1,7 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
+var cron = require('node-cron');
+
 var controller = {
-
-
     getByUser: function (req, res) {
         if (req.body) {
             DFMSubscription.getByUser(req.body, res.callback);
@@ -15,4 +15,43 @@ var controller = {
         }
     }
 };
+cron.schedule('1 * * * * *', function () {
+    DFMSubscription.find({
+        status: 'Active'
+    }, function (err, found) {
+        if (err) {
+            callback(err, null);
+        } else {
+            async.eachSeries(found, function (value, callback1) {
+                    var localDate = moment(value.createdAt);
+                    var currentDate = moment(new Date())
+                    console.log("file doesn't exist", localDate, currentDate, moment(currentDate).isSameOrAfter(localDate));
+                    if (moment(currentDate).isSameOrAfter(localDate)) {
+                        value.status = "Inactive";
+                        value.save(function (err, data) {
+                            if (err) {
+                                console.log("error occured");
+                                callback1("next");
+                            } else {
+                                console.log("value updated");
+                                callback1("next");
+                            }
+                        });
+                    } else {
+                        callback1();
+                    }
+                },
+                function (err, results) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("results", results);
+                        // callback();
+                    }
+                });
+
+        }
+    });
+});
+
 module.exports = _.assign(module.exports, controller);
