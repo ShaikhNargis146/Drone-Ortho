db731d2f64cc287cd8d050cb610f55b7eb7766a7
jagzwohlig <jagruti@wohlig.com>
2017-11-29 14:41:21 +0530

    get transaction details

--- api/controllers/ProductOrdersController.js
+++ api/controllers/ProductOrdersController.js
@@ -181,6 +181,47 @@ var controller = {
 	paymentNotify: function (req, res) {
 		console.log("notify notify");
 		console.log(req.body.payload.id);
+		if (req.body.payload.id) {
+			var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+			merchantAuthenticationType.setName(constants.apiLoginKey);
+			merchantAuthenticationType.setTransactionKey(constants.transactionKey);
+
+			var getRequest = new ApiContracts.GetTransactionDetailsRequest();
+			getRequest.setMerchantAuthentication(merchantAuthenticationType);
+			getRequest.setTransId(req.body.payload.id);
+
+			console.log(JSON.stringify(getRequest.getJSON(), null, 2));
+
+			var ctrl = new ApiControllers.GetTransactionDetailsController(getRequest.getJSON());
+			ctrl.setEnvironment(SDKConstants.endpoint.sandbox);
+
+			ctrl.execute(function () {
+
+				var apiResponse = ctrl.getResponse();
+
+				var response = new ApiContracts.GetTransactionDetailsResponse(apiResponse);
+
+				console.log(JSON.stringify(response, null, 2));
+
+				if (response != null) {
+					if (response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK) {
+						console.log('Transaction Id : ' + response.getTransaction().getTransId());
+						console.log('Transaction Type : ' + response.getTransaction().getTransactionType());
+						console.log('Message Code : ' + response.getMessages().getMessage()[0].getCode());
+						console.log('Message Text : ' + response.getMessages().getMessage()[0].getText());
+					} else {
+						console.log('Result Code: ' + response.getMessages().getResultCode());
+						console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+						console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+					}
+				} else {
+					console.log('Null Response.');
+				}
+
+				// callback(response);
+			});
+		}
+
 	},
 
 	acceptPaymentPage: function (req, res) {
