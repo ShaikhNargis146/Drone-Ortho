ccccceb1d430d57b353e0b9055e188899b36ebb0
jagzwohlig <jagruti@wohlig.com>
2017-12-14 15:52:55 +0530

    auth capture continue

--- api/controllers/ProductOrdersController.js
+++ api/controllers/ProductOrdersController.js
@@ -154,65 +154,137 @@ var controller = {
 
 					//get transaction
 					if (found.transactionId) {
+
 						var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
 						merchantAuthenticationType.setName(constants.apiLoginKey);
 						merchantAuthenticationType.setTransactionKey(constants.transactionKey);
 
-						var getRequest = new ApiContracts.GetTransactionDetailsRequest();
-						getRequest.setMerchantAuthentication(merchantAuthenticationType);
-						getRequest.setTransId(found.transactionId);
+						var payPalType = new ApiContracts.PayPalType();
+						payPalType.setCancelUrl('http://www.merchanteCommerceSite.com/Success/TC25262');
+						payPalType.setSuccessUrl('http://www.merchanteCommerceSite.com/Success/TC25262');
+						payPalType.setPayerID(req.query.PayerID);
 
-						console.log(JSON.stringify(getRequest.getJSON(), null, 2));
+						var paymentType = new ApiContracts.PaymentType();
+						paymentType.setPayPal(payPalType);
 
-						var ctrl = new ApiControllers.GetTransactionDetailsController(getRequest.getJSON());
+						var txnRequest = new ApiContracts.TransactionRequestType();
+						txnRequest.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURECONTINUETRANSACTION);
+						txnRequest.setPayment(paymentType);
+						txnRequest.setAmount(1);
+						txnRequest.setRefTransId(found.transactionId);
+
+						var createRequest = new ApiContracts.CreateTransactionRequest();
+						createRequest.setMerchantAuthentication(merchantAuthenticationType);
+						createRequest.setTransactionRequest(txnRequest);
+
+						console.log(JSON.stringify(createRequest.getJSON(), null, 2));
+
+						var ctrl = new ApiControllers.CreateTransactionController(createRequest.getJSON());
+
 						ctrl.setEnvironment(SDKConstants.endpoint.production);
 
 						ctrl.execute(function () {
 
 							var apiResponse = ctrl.getResponse();
 
-							var response = new ApiContracts.GetTransactionDetailsResponse(apiResponse);
+							var response = new ApiContracts.CreateTransactionResponse(apiResponse);
 
-							var paymentRes = JSON.parse(JSON.stringify(response, null, 2))
-							console.log("response-----------", paymentRes);
+							console.log(JSON.stringify(response, null, 2));
 
-							if (response != null) {
-								if (response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK) {
-									ProductOrders.update({
-										invoiceNo: response.getTransaction().getOrder().getInvoiceNumber()
-									}, {
-										$set: {
-											paymentResponse: paymentRes,
-											status: 'Paid',
-											transactionId: paymentRes.transaction.transId,
-											transactionDate: paymentRes.transaction.submitTimeUTC
+							if (response !== null) {
+								if (response.getMessages().getResultCode() === ApiContracts.MessageTypeEnum.OK) {
+									if (response.getTransactionResponse().getMessages() !== null) {
+										console.log(".........................................................................");
+										console.log('Successfully created transaction with Transaction ID: ' + response.getTransactionResponse().getTransId());
+										console.log('Payer Id: ' + response.getTransactionResponse().getSecureAcceptance().getPayerID());
+										console.log('Response Code: ' + response.getTransactionResponse().getResponseCode());
+										console.log('Message Code: ' + response.getTransactionResponse().getMessages().getMessage()[0].getCode());
+										console.log('Description: ' + response.getTransactionResponse().getMessages().getMessage()[0].getDescription());
+									} else {
+										console.log(".........................................................................");
+
+										console.log('Failed Transaction.');
+										if (response.getTransactionResponse().getErrors() !== null) {
+											console.log('Error Code: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorCode());
+											console.log('Error message: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorText());
 										}
-									}).exec(function (err, found) {
-										if (err) {
-											console.log(err);
-										} else if (_.isEmpty(found)) {
-											console.log("noDataound");
-										} else {
-											console.log('Transaction Id : ' + response.getTransaction().getTransId());
-											console.log('Transaction Type : ' + response.getTransaction().getTransactionType());
-											console.log('Message Code : ' + response.getMessages().getMessage()[0].getCode());
-											console.log('Message Text : ' + response.getMessages().getMessage()[0].getText());
-											res.redirect("http://unifli.aero/thankyou/" + invoiceNum);
-										}
-
-									});
-
+									}
 								} else {
-									console.log('Result Code: ' + response.getMessages().getResultCode());
-									console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
-									console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+									console.log(".........................................................................");
+
+									console.log('Failed Transaction. ');
+									if (response.getTransactionResponse() !== null && response.getTransactionResponse().getErrors() !== null) {
+										console.log('Error Code: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorCode());
+										console.log('Error message: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorText());
+									} else {
+										console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+										console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+									}
 								}
 							} else {
 								console.log('Null Response.');
 							}
 
-							// callback(response);
 						});
+						// var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+						// merchantAuthenticationType.setName(constants.apiLoginKey);
+						// merchantAuthenticationType.setTransactionKey(constants.transactionKey);
+
+						// var getRequest = new ApiContracts.GetTransactionDetailsRequest();
+						// getRequest.setMerchantAuthentication(merchantAuthenticationType);
+						// getRequest.setTransId(found.transactionId);
+
+						// console.log(JSON.stringify(getRequest.getJSON(), null, 2));
+
+						// var ctrl = new ApiControllers.GetTransactionDetailsController(getRequest.getJSON());
+						// ctrl.setEnvironment(SDKConstants.endpoint.production);
+
+						// ctrl.execute(function () {
+
+						// 	var apiResponse = ctrl.getResponse();
+
+						// 	var response = new ApiContracts.GetTransactionDetailsResponse(apiResponse);
+
+						// 	var paymentRes = JSON.parse(JSON.stringify(response, null, 2))
+						// 	console.log("response-----------", paymentRes);
+
+						// 	if (response != null) {
+						// 		if (response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK) {
+						// 			ProductOrders.update({
+						// 				invoiceNo: response.getTransaction().getOrder().getInvoiceNumber()
+						// 			}, {
+						// 				$set: {
+						// 					paymentResponse: paymentRes,
+						// 					status: 'Paid',
+						// 					transactionId: paymentRes.transaction.transId,
+						// 					transactionDate: paymentRes.transaction.submitTimeUTC
+						// 				}
+						// 			}).exec(function (err, found) {
+						// 				if (err) {
+						// 					console.log(err);
+						// 				} else if (_.isEmpty(found)) {
+						// 					console.log("noDataound");
+						// 				} else {
+						// 					console.log('Transaction Id : ' + response.getTransaction().getTransId());
+						// 					console.log('Transaction Type : ' + response.getTransaction().getTransactionType());
+						// 					console.log('Message Code : ' + response.getMessages().getMessage()[0].getCode());
+						// 					console.log('Message Text : ' + response.getMessages().getMessage()[0].getText());
+						// 					res.redirect("http://unifli.aero/thankyou/" + invoiceNum);
+						// 				}
+
+						// 			});
+
+						// 		} else {
+						// 			console.log('Result Code: ' + response.getMessages().getResultCode());
+						// 			console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+						// 			console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+						// 		}
+						// 	} else {
+						// 		console.log('Null Response.');
+						// 	}
+
+						// 	// callback(response);
+						// });
 					} else {
 
 					}
@@ -376,7 +448,7 @@ var controller = {
 						paymentType.setPayPal(payPalType);
 
 						var transactionRequestType = new ApiContracts.TransactionRequestType();
-						transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURECONTINUETRANSACTION);
+						transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
 						transactionRequestType.setPayment(paymentType);
 						transactionRequestType.setAmount(1);
 
