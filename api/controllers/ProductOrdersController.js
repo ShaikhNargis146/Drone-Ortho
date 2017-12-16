3e30f114109e6bbfcf08330a162a5dcf617011ea
jagzwohlig <jagruti@wohlig.com>
2017-12-16 18:21:34 +0530

    some chnages

--- api/controllers/ProductOrdersController.js
+++ api/controllers/ProductOrdersController.js
@@ -431,7 +431,7 @@ var controller = {
 						transactionBillTo.setFirstName(data.billingAddress.fname);
 						transactionBillTo.setLastName(data.billingAddress.lname);
 						transactionBillTo.setCompany(data.billingAddress.comapny);
-						transactionBillTo.setAddress(data.billingAddress.address);
+						transactionBillTo.setAddress(data.billingAddress.streetAddress);
 						transactionBillTo.setCity(data.billingAddress.city);
 						transactionBillTo.setState(data.billingAddress.state);
 						transactionBillTo.setZip(data.billingAddress.zip);
@@ -445,7 +445,7 @@ var controller = {
 						transactionShippTo.setFirstName(data.shippingAddress.fname);
 						transactionShippTo.setLastName(data.shippingAddress.lname);
 						transactionShippTo.setCompany(data.shippingAddress.comapny);
-						transactionShippTo.setAddress(data.shippingAddress.address);
+						transactionShippTo.setAddress(data.shippingAddress.streetAddress);
 						transactionShippTo.setCity(data.shippingAddress.city);
 						transactionShippTo.setState(data.shippingAddress.state);
 						transactionShippTo.setZip(data.shippingAddress.zip);
@@ -559,7 +559,7 @@ var controller = {
 						transactionBillTo.setFirstName(data.billingAddress.fname);
 						transactionBillTo.setLastName(data.billingAddress.lname);
 						transactionBillTo.setCompany(data.billingAddress.comapny);
-						transactionBillTo.setAddress(data.billingAddress.address);
+						transactionBillTo.setAddress(data.billingAddress.streetAddress);
 						transactionBillTo.setCity(data.billingAddress.city);
 						transactionBillTo.setState(data.billingAddress.state);
 						transactionBillTo.setZip(data.billingAddress.zip);
@@ -573,7 +573,7 @@ var controller = {
 						transactionShippTo.setFirstName(data.shippingAddress.fname);
 						transactionShippTo.setLastName(data.shippingAddress.lname);
 						transactionShippTo.setCompany(data.shippingAddress.comapny);
-						transactionShippTo.setAddress(data.shippingAddress.address);
+						transactionShippTo.setAddress(data.shippingAddress.streetAddress);
 						transactionShippTo.setCity(data.shippingAddress.city);
 						transactionShippTo.setState(data.shippingAddress.state);
 						transactionShippTo.setZip(data.shippingAddress.zip);
--- api/controllers/constants.js
+++ api/controllers/constants.js
@@ -4,4 +4,4 @@
 module.exports.apiLoginKey = '4SpEa583';
 
 // module.exports.transactionKey = '88Br8YR6nrk464Su';
-module.exports.transactionKey = '276A287A3wpgH4ng';
+module.exports.transactionKey = '7mM8r396AXkk6V4N';
