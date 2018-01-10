7cf03915623e5688fad2a08e6ff6f621ad028b73
kothavaleganesh <ganesh.wohlig@gmail.com>
2018-01-10 18:53:21 +0530

    path changed

--- api/controllers/MissionController.js
+++ api/controllers/MissionController.js
@@ -645,7 +645,7 @@ var controller = {
         var name = req.param("filename");
         var id = req.param("id");
         var files = [];
-        files = ["/mymountpoint/" + id + "/" + name + "/3_dsm_ortho/2_mosaic/" + name + "_transparent_mosaic_group1.tif", "C:/Users/unifli/Documents/pix4d/" + name + "/3_dsm_ortho/2_mosaic/" + name + "_transparent_mosaic_group1.tfw"]
+        files = ["/mymountpoint/" + id + "/" + name + "/3_dsm_ortho/2_mosaic/" + name + "_transparent_mosaic_group1.tif", "/mymountpoint/" + id + "/" + name + "/3_dsm_ortho/2_mosaic/" + name + "_transparent_mosaic_group1.tfw"]
         async.eachSeries(files, function (image, callback) {
             // request(global["env"].realHost + '/api/upload/readFile?file=' + image).pipe(fs.createWriteStream(image)).on('finish', function (images) {
             // JSZip generates a readable stream with a "end" event,
