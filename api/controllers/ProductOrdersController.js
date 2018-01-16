ca94b97658056329154fa5724a2cd765ab6884fd
Aditya <aditya.ghag@wohlig.com>
2018-01-16 12:01:52 +0530

    recursive payment test

--- api/controllers/ProductOrdersController.js
+++ api/controllers/ProductOrdersController.js
@@ -310,10 +310,100 @@ var controller = {
 		res.redirect("http://unifli.aero/sorry");
 	},
 
+	// createCustomerProfile: function (req, res) {
+
+	// 	var invoiceUserId = {};
+	// 	console.log("...........................Payment Responce..........................");
+	// 	console.log(req.allParams());
+	// 	console.log(req.query);
+	// 	console.log("...........................Payment Responce..........................");
+
+	// 	ProductOrders.findOne({
+	// 		invoiceNo: req.allParams().invoiceNumber
+	// 	}).lean().exec(function (err, found) {
+	// 		console.log("--------------",found)			
+	// 		if (err || _.isEmpty(found)) {} else {
+	// 				console.log("--------------",found)
+	// 				var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+	// 				console.log("constants.apiLoginKey",constants.apiLoginKey)
+	// 				console.log("constants.apiLoginKey",constants.transactionKey)
+	// 				merchantAuthenticationType.setName(constants.apiLoginKey);
+	// 				merchantAuthenticationType.setTransactionKey(constants.transactionKey);
+
+	// 				var creditCard = new ApiContracts.CreditCardType();
+	// 				creditCard.setCardNumber('4242424242424242');
+	// 				creditCard.setExpirationDate('0822');
+
+	// 				var paymentType = new ApiContracts.PaymentType();
+	// 				paymentType.setCreditCard(creditCard);
+
+	// 				var customerAddress = new ApiContracts.CustomerAddressType();
+	// 				customerAddress.setFirstName('test');
+	// 				customerAddress.setLastName('scenario');
+	// 				customerAddress.setAddress('123 Main Street');
+	// 				customerAddress.setCity('Bellevue');
+	// 				customerAddress.setState('WA');
+	// 				customerAddress.setZip('98004');
+	// 				customerAddress.setCountry('USA');
+	// 				customerAddress.setPhoneNumber('000-000-0000');
+
+	// 				var customerPaymentProfileType = new ApiContracts.CustomerPaymentProfileType();
+	// 				customerPaymentProfileType.setCustomerType(ApiContracts.CustomerTypeEnum.INDIVIDUAL);
+	// 				customerPaymentProfileType.setPayment(paymentType);
+	// 				customerPaymentProfileType.setBillTo(customerAddress);
+
+	// 				var paymentProfilesList = [];
+	// 				paymentProfilesList.push(customerPaymentProfileType);
+
+	// 				var customerProfileType = new ApiContracts.CustomerProfileType();
+	// 				customerProfileType.setMerchantCustomerId('M_' + utils.getRandomString('cust'));
+	// 				customerProfileType.setDescription('Profile description here');
+	// 				customerProfileType.setEmail(utils.getRandomString('cust') + '@anet.net');
+	// 				customerProfileType.setPaymentProfiles(paymentProfilesList);
+
+	// 				var createRequest = new ApiContracts.CreateCustomerProfileRequest();
+	// 				createRequest.setProfile(customerProfileType);
+	// 				createRequest.setValidationMode(ApiContracts.ValidationModeEnum.TESTMODE);
+	// 				createRequest.setMerchantAuthentication(merchantAuthenticationType);
+
+	// 				//pretty print request
+	// 				//console.log(JSON.stringify(createRequest.getJSON(), null, 2));
+
+
+	// 				var ctrl = new ApiControllers.CreateCustomerProfileController(createRequest.getJSON());
+
+	// 				ctrl.execute(function () {
+
+	// 					var apiResponse = ctrl.getResponse();
+
+	// 					var response = new ApiContracts.CreateCustomerProfileResponse(apiResponse);
+
+	// 					//pretty print response
+	// 					//console.log(JSON.stringify(response, null, 2));
+
+	// 					if (response != null) {
+	// 						if (response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK) {
+	// 							console.log('Successfully created a customer profile with id: ' + response.getCustomerProfileId());
+	// 						} else {
+	// 							console.log('Result Code: ' + response.getMessages().getResultCode());
+	// 							console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+	// 							console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+	// 						}
+	// 					} else {
+	// 						console.log('Null response received');
+	// 					}
+
+	// 					res.callback(response);
+	// 				})
+	// 			}
+	// 	});
+	// },
+
 	/**
 	 * Webhook Funtion
 	 * This function is initated in authorized.net.
 	 */
+	
 	paymentNotify: function (req, res) {
 		console.log("notify notify");
 		console.log(req.body);
@@ -643,7 +733,7 @@ var controller = {
 						console.log(JSON.stringify(getRequest.getJSON(), null, 2));
 
 						var ctrl = new ApiControllers.GetHostedPaymentPageController(getRequest.getJSON());
-						ctrl.setEnvironment(SDKConstants.endpoint.production);
+						ctrl.setEnvironment(SDKConstants.endpoint.sandbox);
 
 						ctrl.execute(function () {
 
@@ -887,6 +977,7 @@ var controller = {
 			});
 		}
 	},
+
 	paymentIdGenerate: function (req, res) {
 		if (req.body) {
 			ProductOrders.paymentIdGenerate(req.body, res.callback);
