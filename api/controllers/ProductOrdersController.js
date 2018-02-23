1b69bbef9f4d901edb59ee7ff58e1103fb2d50e8
Aditya <aditya.ghag@wohlig.com>
2018-02-23 19:14:54 +0530

    chnages

--- api/controllers/ProductOrdersController.js
+++ api/controllers/ProductOrdersController.js
@@ -195,8 +195,8 @@ var controller = {
 						var txnRequest = new ApiContracts.TransactionRequestType();
 						txnRequest.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURECONTINUETRANSACTION);
 						txnRequest.setPayment(paymentType);
-						// txnRequest.setAmount(found.totalAmount);
-						txnRequest.setAmount(1);						
+						txnRequest.setAmount(found.totalAmount);
+						// txnRequest.setAmount(1);						
 						txnRequest.setRefTransId(found.transactionId);
 
 						var createRequest = new ApiContracts.CreateTransactionRequest();
@@ -842,8 +842,8 @@ var controller = {
 						var transactionRequestType = new ApiContracts.TransactionRequestType();
 						transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
 						transactionRequestType.setPayment(paymentType);
-						// transactionRequestType.setAmount(data.totalAmount);
-						transactionRequestType.setAmount(1);
+						transactionRequestType.setAmount(data.totalAmount);
+						// transactionRequestType.setAmount(1);
 						
 
 
@@ -1015,8 +1015,8 @@ var controller = {
 
 						var transactionRequestType = new ApiContracts.TransactionRequestType();
 						transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
-						// transactionRequestType.setAmount(data.totalAmount);
-						transactionRequestType.setAmount(1);
+						transactionRequestType.setAmount(data.totalAmount);
+						// transactionRequestType.setAmount(1);
 						
 
 						var transactionOrderType = new ApiContracts.OrderType();
